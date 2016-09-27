3b8b37c4643eab83c749d0b079c82ac3740a7b8e
alinewm <alinewada@gmail.com>
2016-09-27 16:32:06 -0400

    New Trie implementation

--- tests.js
+++ tests.js
@@ -1,17 +1,46 @@
 (function() {
   mocha.setup("bdd");
   var assert = chai.assert;
+  var expect = chai.expect;
+
+  beforeEach(function() {
+    testTrie = Trie();
+  });
 
   describe("Trie", function() {
     describe("autocomplete", function() {
-      it("should do X", function() {
-        assert.equal(2, 2);
+      it("should return no results if search term isn't in the dictionary", function() {
+        assert.lengthOf(testTrie.autocomplete('zzz'), 0);
       });
 
-      it("should do Y", function() {
-        assert.equal(3, 1 + 2);
+      it("should return the full word itself and any other words that begins with it", function() {
+        ['foot', 'football', 'footer'].forEach(function(word) { testTrie.addWord(word) });
+        expect(testTrie.autocomplete('foot')).eql(['foot', 'football', 'footer']);
       });
+
+      it("should return results in lexicographical order", function() {
+        input = ['a', 'ab', 'ac'];
+        input.forEach(function(substring) {
+          testTrie.addWord(substring);
+        })
+        suggestions = testTrie.autocomplete('a');
+        expect(suggestions).eql(input);
+      });
+
+      it("should return at most 10 results", function() {
+        'asdfwqertghjdgf'.split('').forEach(function(letter) {
+          testTrie.addWord('a' + letter);
+        });
+        assert.isAtMost(testTrie.autocomplete('a').length,10);
+      })
     });
+
+    describe("addWord", function() {
+      it("should add word to the dictionary", function() {
+        testTrie.addWord('hello');
+        assert.include(testTrie.autocomplete('h'), 'hello');
+      });
+    })
   });
 
   mocha.run();
--- trie.js
+++ trie.js
@@ -1,25 +1,111 @@
 var Trie = function() {
-  // This is an awful way to create an object!
-  // You should use the Object.create + Object.freeze technique we described in class.
-  return {
-    "addWord": function(word) {
-      // This is a bad implementation, so we're going to do nothing in this function.
-    },
 
-    "autocomplete": function(prefix) {
-      // This is a terrible O(n^2) (n = dictionary length)
-      // algorithm that runs every time a letter is typed.
-      return WORDS
-        .filter(function(word, index, words) {
-          // First remove duplicates.
-          return words.indexOf(word) === index;
-        })
-        .filter(function(word) {
-          // Now find matching words.
-          return word.indexOf(prefix) === 0;
-        })
-        .sort()
-        .slice(0, 10);
+  var Node = function(value, substring) {
+    var that = {};
+    var that = Object.create(Node.prototype);
+    var children = [];
+    var fullWord = false;
+    that.getValue = function() {
+      return value;
+    }
+    that.getSubstring = function() {
+      return substring;
+    }
+    that.addChild = function(child) {
+      children.push(child);
+      return that;
+    }
+    that.getChildren = function() {
+      return children;
+    }
+    that.hasChildren = function() {
+      return children.length>0;
+    }
+    that.setFullWord = function() {
+      fullWord = true;
+      return that;
+    }
+    that.isFullWord = function() {
+      return fullWord;
+    }
+    Object.freeze(that);
+    return that;
+  }
+
+  var that = {};
+  var that = Object.create(Trie.prototype);
+  var root = Node('','');
+
+  that.addWord = function(word) {
+    var parent = root;
+    word.split("").forEach(function(character) {
+      matches = (parent.getChildren().filter(function(child) {
+        return child.getValue()===character;
+      }))
+      if (matches.length == 0) {
+        var newChild = Node(character, parent.getSubstring() + character);
+        parent.addChild(newChild);
+        parent = newChild;
+      } else {
+        parent = matches[0];
+      }
+    });
+    parent.setFullWord();
+  };
+
+  that.autocomplete = function(prefix) {    
+
+    findNodeByName = function(word) {
+      parent = root;
+      word.split("").forEach(function(character) {
+        if(parent == null) { return };
+        matches = parent.getChildren().filter(function(child) {
+          return child.getValue() === character;
+        });
+        if(matches.length > 0) { parent = matches[0] }
+        else { parent = null }
+      });
+      return parent;
+    };
+
+    listFullWordDescendants = function(parent=root) {
+      // Given the parent node, corresponding to the prefix, return an array of full words
+      // else return an empty array
+
+      // Traverse the nodes, if the node is a full word, populate into the array and continue traversing
+      // if it's not a full word, continue traversing
+      // Output: an array of words
+
+      if (!parent.hasChildren()) { return []; }
+      
+      results = parent.getChildren()
+      .map(function(child) { 
+        if (child.isFullWord()) {
+          return listFullWordDescendants(child).concat(child.getSubstring());
+        } else {
+          return listFullWordDescendants(child);
+        }
+      })
+      .reduce(function(reduced=[], toReduce) { 
+        res = reduced.concat(toReduce);   
+        return res;
+      });
+      return results;
+    };
+
+    var parent = findNodeByName(prefix);
+    if (parent) {
+      res = listFullWordDescendants(parent);
+      if (parent.isFullWord()) {
+        res.push(parent.getSubstring());
+      }
+      return res.sort().slice(0, 10);
+    } else {
+      console.log('parent not found')
+      return [];
     }
   }
-};
+
+  Object.freeze(that);
+  return that;
+}
